22ce612766fb171c2264cad06a31d270bdcb184b
LovkashGarg <aggarwallovkesh55@gmail.com>
2024-10-12 22:51:31 +0530

    deployment done

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/url/', {
+      const response = await fetch('https://trunc8-backend.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json', // Send as JSON
